715647491074014d583a0b07f956f42aebff7bec
Ettore Raimondi <ettore495@gmail.com>
2021-03-10 13:05:17 +0100

    Added register page, WIP adding router

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Logo from '../shared/logo/logo'
-import './sign-in.scss'
+import './register.scss'
 
-function SignIn() {
+function Register() {
   return (
     <div className="sign-in">
       <form>
         <Logo />
 
+        <input type="first-name" id="inputFirstName" className="form-control" placeholder="First name" required />
+        <input type="last-name" id="inputLastName" className="form-control" placeholder="Last name" required />
         <input type="email" id="inputEmail" className="form-control" required placeholder="Email address" autoFocus />
         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-
-        <p className="mt-4">Don't have an account? <a href="/sign-up">Sign up</a></p>
 
         <button className="btn btn-lg btn-primary btn-block mt-4" type="submit">Sign in</button>
       </form>
@@ -19,4 +19,4 @@ function SignIn() {
   );
 }
 
-export default SignIn;
+export default Register;
