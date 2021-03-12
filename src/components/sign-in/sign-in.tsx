6e82d20282d368da50d1335ebbbc176e9020b5ba
Ettore Raimondi <ettore495@gmail.com>
2021-03-12 20:50:01 +0100

    Added form validation to sign in page

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import Logo from '../shared/logo/logo'
-import './sign-in.scss'
+import React, { useState } from "react";
+import { Button, Col, Form } from "react-bootstrap";
+import Logo from "../shared/logo/logo";
+import "./sign-in.scss";
 
 function SignIn() {
+  const [validated, setValidated] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (event: any) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className="sign-in">
-      <form>
-        <Logo />
+      <Logo />
 
-        <h4 className="mb-4">Sign in</h4>
+      <h4 className="mb-4">Sign in</h4>
 
-        <input type="email" id="inputEmail" className="form-control" required placeholder="Email address" autoFocus />
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-
-        <p className="mt-4">Don't have an account? <a href="/sign-up">Sign up</a></p>
-
-        <button className="btn btn-md btn-primary btn-block mt-4" type="submit">Sign in</button>
-      </form>
+      <Form noValidate validated={validated} onSubmit={handleRegister}>
+        <Form.Row>
+          <Form.Group as={Col} md="12" controlId="validationCustom01">
+            <Form.Control
+              required
+              type="email"
+              placeholder="Email"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              autoFocus
+            />
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoFocus
+            />
+          </Form.Group>
+        </Form.Row>
+        <p className="mt-1  ">
+          Don't have an account? <a href="/sign-up">Sign up</a>
+        </p>
+        <Button className="w-100" type="submit">
+          Sign in
+        </Button>
+      </Form>
     </div>
   );
 }
