d3254785f3a56ed8ccc65cb35ceaced3ca124d0f
Ettore Raimondi <ettore495@gmail.com>
2021-03-12 21:09:35 +0100

    Integrated login with api

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 import { Button, Col, Form } from "react-bootstrap";
 import Logo from "../shared/logo/logo";
+import { useHistory } from "react-router-dom";
+import { LOGIN } from "../../mutations/auth/login";
 import "./sign-in.scss";
 
 function SignIn() {
+  const history = useHistory();
+
+  const [loginUser, { loading, error, data }] = useMutation(LOGIN);
+
   const [validated, setValidated] = useState(false);
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleRegister = (event: any) => {
+  const handleLogin = (event: any) => {
     event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -16,6 +23,19 @@ function SignIn() {
     }
 
     setValidated(true);
+
+    loginUser({
+      variables: {
+        username: userName,
+        password: password,
+      },
+    })
+      .then((result) => {
+        history.push("/home/movies");
+      })
+      .catch((error) => {
+        alert("invalid username or password");
+      });
   };
 
   return (
@@ -24,7 +44,7 @@ function SignIn() {
 
       <h4 className="mb-4">Sign in</h4>
 
-      <Form noValidate validated={validated} onSubmit={handleRegister}>
+      <Form noValidate validated={validated} onSubmit={handleLogin}>
         <Form.Row>
           <Form.Group as={Col} md="12">
             <Form.Control
